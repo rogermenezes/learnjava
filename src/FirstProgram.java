92fd0d1056e0b4fd9dfeacfea3a0f3a946214f25
Roger Menezes <rmenezes@netflix.com>
2025-05-03 17:28:45 -0700

    change file name

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -1,8 +1,8 @@
 
-public class Main {
+public class FirstProgram {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!!!");
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello, Serena!");
         }
